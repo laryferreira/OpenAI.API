c97650da718e60b5ebfa842ead1b364e9ef6baea
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-02-02 20:55:09 -0300

    Update dalle.py

--- dalle.py
+++ dalle.py
@@ -1,20 +1,20 @@
-import openai 
-import datetime 
-from openai.error import InvalidRequestError
-import webbrowser
-from base64 import b64decode
+import openai # main library
+import datetime # these libraries will be required as the code expands. do it step by step
+from openai.error import InvalidRequestError # except
+import webbrowser # generating the image in a new web browser
+from base64 import b64decode # jpg 
 
 
-def image_gen(prompt, n=1, size = '512x512', output_format = 'url'):
+def image_gen(prompt, n=1, size = '512x512', output_format = 'url'): # function that takes the user's prompt, number of images, size of images and web output
     try:
-        images = []
+        images = [] #empty list
         response = openai.Image.create(
-            prompt = prompt,
+            prompt = prompt, #required variables. see documentation
             n = n,
             size = size,
             response_format = output_format
         )
-        if output_format == 'url':
+        if output_format == 'url': 
             for image in response['data']:
                 images.append(image.url)
 
@@ -22,9 +22,9 @@ def image_gen(prompt, n=1, size = '512x512', output_format = 'url'):
             for image in response['data']:
                 images.append(image.b64_json)
 
-        return {'created': datetime.datetime.fromtimestamp(response['created']), 'images': images}
+        return {'created': datetime.datetime.fromtimestamp(response['created']), 'images': images} # return datetime of creation and image
 
-    except InvalidRequestError as e:
+    except InvalidRequestError as e: # new variable
         print(e)
 
 
@@ -32,7 +32,7 @@ API_KEY = # your api
 openai.api_key = API_KEY
 
 SIZES = ('1024x1024', '512x512', '256x256')
-prompt = str(input("What do you wanna do today? "))
+prompt = str(input("What do you wanna do today? ")) # use your imagination
 choice = int(input("Enter 0 to URL and 1 to save as JPG"))
 if choice == 0:
     # generate url image
@@ -47,5 +47,5 @@ elif choice == 1:
     response = image_gen(prompt, n=2, size=SIZES[1], output_format='b64_json')
     prefix = 'openai_gen'
     for index, image in enumerate(response['images']):
-        with open(f"{prefix}_{index}.jpg","wb") as f:
+        with open(f"{prefix}_{index}.jpg","wb") as f: #this is preparing the browser for jpg
             f.write(b64decode(image))
